a465ab1d84b5c57a50138d3ae42c8328ef50c8a4
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-06-02 18:41:14 -0700

    Time: 158 ms (63.95%), Space: 135.4 MB (5.41%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -6,22 +6,25 @@ class NumMatrix {
     public NumMatrix(int[][] matrix) {
         cache = new int[matrix.length][matrix[0].length];
         int sum;
-        for(int i = 0; i < matrix.length; i++){
+        for(int i = matrix.length - 1; i >= 0; i--){
             sum = 0;
             for(int j = matrix[i].length - 1; j >= 0; j--){
                 sum += matrix[i][j];
                 cache[i][j] = sum;
+                if(i + 1 < matrix.length)
+                    cache[i][j] += cache[i + 1][j];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        ans = 0;
-        for(int i = row1; i <= row2; i++){
-            ans += cache[i][col1];
-            if(col2 + 1 < cache[0].length)
-                ans -= cache[i][col2 + 1];
-        }
+        ans = cache[row1][col1];
+        if(row2 + 1 < cache.length)
+            ans -= cache[row2 + 1][col1];
+        if(col2 + 1 < cache[0].length)
+            ans -= cache[row1][col2 + 1];
+        if(row2 + 1 < cache.length && col2 + 1 < cache[0].length)
+            ans += cache[row2 + 1][col2 + 1];
         return ans;
     }
 }
