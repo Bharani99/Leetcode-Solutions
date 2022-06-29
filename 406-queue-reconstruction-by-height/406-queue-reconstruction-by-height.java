dba4c4f3ff2d7282f0a0b806e7602beda9d897d7
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-06-29 15:25:07 -0700

    Time: 11 ms (55.85%), Space: 54.6 MB (60.17%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
@@ -1,29 +1,13 @@
 class Solution {
-    
-    public void putInFirst(int[][] out, int[] vals){
-        int free = 0;
-        for(int i = 0; i < out.length; i++){
-            if(vals[1] == free && out[i][0] == -1){
-                out[i] = vals;
-                break;
-            }
-            else if( (out[i][0] == -1) || out[i][0] == vals[0]) free++;
-        }
-    }
-    
     public int[][] reconstructQueue(int[][] people) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
+        Arrays.sort(people, (a,b) -> {
             if(a[0] == b[0]) return Integer.compare(a[1], b[1]);
-            else return Integer.compare(a[0], b[0]);
+            else return Integer.compare(b[0], a[0]);
         });
-        int[][] output = new int[people.length][2];
-        for(int [] a : output) Arrays.fill(a, -1);
-        int[] curr;
-        for(int[] person : people) pq.add(person);
-        while(!pq.isEmpty()){
-            curr = pq.poll();
-            putInFirst(output, curr);
+        List<int[]> out = new ArrayList<>(people.length);
+        for(int[] curr : people){
+            out.add(curr[1], curr);
         }
-        return output;
+        return out.toArray(new int[0][]);
     }
 }
