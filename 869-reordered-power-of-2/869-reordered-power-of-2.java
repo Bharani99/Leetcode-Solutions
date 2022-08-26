e607f1d70dfc49babdecd65ef723ff1123cd0084
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-08-26 01:20:21 -0700

    Time: 1 ms (100.00%), Space: 40.2 MB (86.00%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,25 +1,26 @@
 class Solution {
-    public String getHash(int num){
+    public boolean checkAnagram(int num, int curr){
         int[] hash = new int[10];
-        StringBuilder sb = new StringBuilder();
         while(num > 0){
             hash[num % 10]++;
             num /= 10;
         }
-        for(int i = 0; i < 10; i++){
-            sb.append("#" + i + "#" + hash[i] + "#");
+        while(curr > 0){
+            if(hash[curr % 10] == 0) return false;
+            hash[curr % 10] --;
+            curr /= 10;
         }
-        return sb.toString();
+        return true;
     }
         
-    public boolean checkValues(int digits, String hash){
+    public boolean checkValues(int digits, int num){
         int[] pattern = new int[]{0, 4, 7};
         int[] end = new int[]{4, 3, 3};
         int bucket = (digits - 1) / 3;
         int start = (digits - 1) % 3;
         int index = bucket * 10 + pattern[start];
         for(int i = 0; i < end[start]; i++){
-            if(getHash(1 << index++).equals(hash)) 
+            if(checkAnagram(1 << index++, num)) 
                 return true;
         }
         return false;
@@ -33,6 +34,6 @@ class Solution {
             temp /= 10;
             numOfDigits++;
         }
-        return checkValues(numOfDigits, getHash(n));
+        return checkValues(numOfDigits, n);
     }
 }
