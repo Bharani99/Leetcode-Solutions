14a1052a229606dc000ca6be85e564b0c567bc58
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-08-27 05:15:12 -0700

    Time: 2782 ms (5.17%), Space: 43.6 MB (81.43%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.java
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.java
@@ -1,27 +1,56 @@
 class Solution {
-    public int maxSumSubmatrix(int[][] matrix, int k) {
-        int m = matrix.length, n = matrix[0].length;
-        int ans = Integer.MIN_VALUE;
-        for (int r1 = 0; r1 < m; ++r1) {
-            int[] arr = new int[n]; // arr[i] is sum(matrix[r1][c]...matrix[r2][c])
-            for (int r2 = r1; r2 < m; ++r2) {
-                for (int c = 0; c < n; ++c) arr[c] += matrix[r2][c];
-                ans = Math.max(ans, maxSumSubArray(arr, n, k));
-            }
-        }
+    int ans = Integer.MIN_VALUE;
+    
+    public int getColVal(int rowStart, int rowEnd, int col, int[][] colSum){
+        int ans = colSum[rowEnd][col];
+        if(rowStart > 0) ans -= colSum[rowStart - 1][col];
         return ans;
     }
-    int maxSumSubArray(int[] arr, int n, int k) { // O(N * logN)
-        TreeSet<Integer> bst = new TreeSet<>();
-        bst.add(0);
-        int ans = Integer.MIN_VALUE;
-        for (int i = 0, right = 0; i < n; ++i) {
-            right += arr[i];
-            Integer left = bst.ceiling(right - k); // right - left <= k -> left >= right - k
-            if (left != null) {
-                ans = Math.max(ans, right - left);
+    
+    public int getRowVal(int colStart, int colEnd, int row, int[][] rowSum){
+        int ans = rowSum[row][colEnd];
+        if(colStart > 0) ans -= rowSum[row][colStart - 1];
+        return ans;
+    }
+    
+    public void getMaxSum(int startRow, int startCol, int endRow, int endCol, int[][] rowSum, int[][] colSum, int currSum, int k, int[][] visited){
+        if(currSum <= k && currSum > ans) ans = currSum;
+        if(visited[endRow][endCol] == 1) return;
+        visited[endRow][endCol] = 1;
+        if(endCol + 1 < rowSum[0].length)
+            getMaxSum(startRow, startCol, endRow, endCol + 1, rowSum, colSum, currSum + getColVal(startRow, endRow, endCol + 1, colSum), k, visited);
+        
+        if(endRow + 1 < rowSum.length)
+            getMaxSum(startRow, startCol, endRow + 1, endCol, rowSum, colSum, currSum + getRowVal(startCol, endCol, endRow + 1, rowSum), k, visited);
+                
+    }
+    
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] rowSum = new int[m][n];
+        int[][] colSum = new int[m][n];
+        int sum = 0;
+        for(int i = 0; i < m; i ++){
+            sum = 0;
+            for(int j = 0; j < n; j++){
+                sum += matrix[i][j];
+                rowSum[i][j] = sum;
             }
-            bst.add(right);
+        }
+        
+        for(int i = 0; i < n; i ++){
+            sum = 0;
+            for(int j = 0; j < m; j++){
+                sum += matrix[j][i];
+                colSum[j][i] = sum;
+            }
+        }
+        int[][] visited = new int[m][n];
+        for(int i = 0; i < m; i ++){
+            for(int j = 0; j < n; j++){
+                visited = new int[m][n];
+                getMaxSum(i, j, i, j, rowSum, colSum, matrix[i][j], k, visited);
+            }
         }
         return ans;
     }
