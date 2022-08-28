091fd876dcf90a139a23447ba820937cc0aed8c5
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-08-27 21:59:53 -0700

    Time: 10 ms (61.20%), Space: 47.5 MB (76.04%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,16 +1,32 @@
 class Solution {
-    public int[][] diagonalSort(int[][] A) {
-        int m = A.length, n = A[0].length;
-        HashMap<Integer, PriorityQueue<Integer>> d = new HashMap<>();
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                d.putIfAbsent(i - j, new PriorityQueue<>());
-                d.get(i - j).add(A[i][j]);
+    public int[][] diagonalSort(int[][] mat) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int m = mat.length, n = mat[0].length, j, k;
+        for(int i = 0; i < m; i++){
+            j = i;
+            k = 0;
+            while(j < m && k < n){
+                pq.add(mat[j++][k++]);
             }
+                        
+            j = i;
+            k = 0;
+            while(j < m && k < n)
+                mat[j++][k++] = pq.poll();
         }
-        for (int i = 0; i < m; ++i)
-            for (int j = 0; j < n; ++j)
-                A[i][j] = d.get(i - j).poll();
-        return A;
+        
+        for(int i = 1; i < n; i++){
+            j = 0;
+            k = i;
+            while(j < m && k < n){
+                pq.add(mat[j++][k++]);
+            }
+                        
+            j = 0;
+            k = i;
+            while(j < m && k < n)
+                mat[j++][k++] = pq.poll();
+        }
+        return mat;
     }
 }
