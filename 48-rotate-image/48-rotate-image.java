a46b707dcd2827e0d4cbb45c6807fa7fa8a72313
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-08-29 19:14:14 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (80.48%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,25 +1,19 @@
 class Solution {
-    
-    public void rotate(int[][] matrix, int i, int j, int x, int y, int m, int n, int mn){
+    public void rotate(int[][] matrix, int i, int j, int x, int y, int n, int mn){
         if(mn == 4) return;
         int val = matrix[i][j];
-        rotate(matrix, x, y, y, n - 1 - x, m, n, mn + 1);
-        matrix[x][y] = val - 3000;
+        rotate(matrix, x, y, y, n - 1 - x, n, mn + 1);
+        matrix[x][y] = val;
     }
     
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(matrix[i][j] <= 1000 && matrix[i][j] >= -1000)
-                    rotate(matrix, i, j, j, n - 1 - i, n, n, 0);
-            }
-        }
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                matrix[i][j] += 3000;
-            }
+        int n = matrix.length, left = 0, right = n - 1, up = 0;
+        while(left < right){
+            for(int i = left; i < right; i++)
+                rotate(matrix, up, i, i, n - 1 - up, n, 0);
+            left ++;
+            right --;
+            up ++;
         }
     }
 }
