d616d8e6e587acf5251d9ddd5bcd9f3f36c4f1ad
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-09-03 01:43:22 -0700

    Time: 6 ms (47.50%), Space: 43.5 MB (23.13%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,17 +1,21 @@
 class Solution {
-    public void genSeq(int n, int k, int curr, int soFar, ArrayList<Integer> list){
-        if(curr < 0 || curr > 9) return;
-        if(n == 0) {
-            list.add(soFar * 10 + curr);
-            return;
-        }
-        genSeq(n - 1, k, curr + k, soFar * 10 + curr, list);
-        if(k != 0) genSeq(n - 1, k, curr - k, soFar * 10 + curr, list);
-    }
     public int[] numsSameConsecDiff(int n, int k) {
-        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
         for(int i = 1; i < 10; i++)
-            genSeq(n - 1, k, i, 0, ans);
-        return ans.stream().mapToInt(i -> i).toArray();
+            q.add(i);
+        n--;
+        int size = 0, temp, digit;
+        while(n > 0){
+            size = q.size();
+            for(int i = 0; i < size; i++){
+                temp = q.poll();
+                digit = temp % 10;
+                if(digit + k < 10) q.add(temp * 10 + digit + k);
+                if(digit - k >= 0 && k != 0) q.add(temp * 10 + digit - k);
+            }
+            n--;
+        }
+        
+        return q.stream().mapToInt(i -> i).toArray();
     }
 }
