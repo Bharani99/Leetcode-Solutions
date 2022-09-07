ff7bbf105ff66d45a9adc42a5647eafaf561c8f1
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-09-07 03:34:11 -0700

    Time: 5 ms (13.73%), Space: 45 MB (32.14%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,23 +1,23 @@
 class Solution {
-    public void perm(int[] nums, HashSet<Integer> ind, HashSet<Integer> temp, List<List<Integer>> output, List<Integer> inner){
+    public void perm(int[] nums, List<Integer> ind, List<List<Integer>> output, List<Integer> inner){
         if(ind.isEmpty()){
             output.add(new ArrayList<>(inner));
             return;
         }
-        temp = new HashSet(ind);
+        List<Integer> temp = new ArrayList(ind);
         for(int index : ind){
             inner.add(nums[index]);
-            temp.remove(index);
-            perm(nums, temp, null, output, inner);
+            temp.remove(new Integer(index));
+            perm(nums, temp, output, inner);
             temp.add(index);
             inner.remove(inner.size() - 1);
         }
     }
     public List<List<Integer>> permute(int[] nums) {
-        HashSet<Integer> ind = new HashSet<>();
+        List<Integer> ind = new ArrayList<>();
         for(int i = 0; i < nums.length; i++)ind.add(i);
         List<List<Integer>> output = new ArrayList<>();
-        perm(nums, ind, null, output, new ArrayList<>());
+        perm(nums, ind, output, new ArrayList<>());
         return output;
     }
 }
