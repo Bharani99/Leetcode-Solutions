c1dba5df8ae5c475c5de991e8f764c47753361d6
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-09-07 18:27:39 -0700

    Time: 2 ms (97.18%), Space: 42.8 MB (96.53%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -1,27 +1,23 @@
 class Solution {
-    public List<String> letterCasePermutation(String s) {
-        s = s.toLowerCase();
-        Queue<StringBuilder> q = new LinkedList<>();
-        int size = 0;
-        q.add(new StringBuilder());
-        StringBuilder temp;
-        
-        for(char c : s.toCharArray()){
-            size = q.size();
-            for(int i = 0; i < size; i++){
-                temp = q.poll();
-                temp.append(c);
-                q.add(new StringBuilder(temp));
-                if(c >= 'a' && c <= 'z'){
-                    temp.deleteCharAt(temp.length() - 1);
-                    temp.append((char)(c - 32));
-                    q.add(temp);
-                }
-            }
+    public void dfs(List<String> output, char[] s, int index, StringBuilder sb){
+        if(index == s.length){
+            output.add(sb.toString());
+            return;
         }
         
+        sb.append(s[index]);
+        dfs(output, s, index + 1, sb);
+        sb.deleteCharAt(index);
+        
+        if(s[index] >= 'a' && s[index] <= 'z'){
+            sb.append((char)(s[index] - 32));
+            dfs(output, s, index + 1, sb);
+            sb.deleteCharAt(index);
+        }
+    }
+    public List<String> letterCasePermutation(String s) {
         List<String> output = new ArrayList<>();
-        while(!q.isEmpty()) output.add(q.poll().toString());
+        dfs(output, s.toLowerCase().toCharArray(), 0, new StringBuilder());
         return output;
     }
 }
