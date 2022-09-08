4bc52d281b6ab27842c94709edd89f24a6c1cd26
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-09-08 02:42:59 -0700

    Time: 0 ms (100.00%), Space: 48.3 MB (89.75%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -4,26 +4,9 @@ class Solution {
         while(left < right){
             if(right - left == 1) return nums[left];
             mid = left + (right - left) / 2;
-            if(mid % 2 == 0) {
-                if(mid + 1 == nums.length || (nums[mid] != nums[mid + 1] && mid > 0 && nums[mid] != nums[mid - 1]))
-                    return nums[mid];
-            }
-            else{
-                if(mid > 0 && mid + 1 < nums.length && (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]))
-                    return nums[mid];
-            }
-            
-            if(mid % 2 == 1){
-                if(nums[mid] == nums[mid - 1])
-                    left = mid + 1;
-                else right = mid;
-            }
-            else{
-                if(nums[mid] != nums[mid - 1])
-                    left = mid + 1;
-                else right = mid;
-            }
-
+            if(mid % 2 == 1) mid--;
+            if(nums[mid] != nums[mid + 1]) right = mid;
+            else left = mid + 2;
         }
         return nums[left];
     }
