56d043c8bf27aa8c2be25807154593ca487f20c8
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-09-13 17:12:09 -0700

    Time: 26 ms (61.70%), Space: 109.9 MB (33.92%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans = 0;
+    public boolean isPalindrome(int[] count){
+        int odd = 1;
+        for(int i = 0; i < 10; i++){
+            if(count[i] % 2 == 1) odd--;
+            if(odd < 0) return false;
+        }
+        return true;
+    }
+    
+    public void pseudoPalindrome(TreeNode curr, int[] count){
+        if(curr == null) return;
+        count[curr.val]++;
+        if(curr.left == null && curr.right == null && isPalindrome(count)) ans++;
+        else{
+            pseudoPalindrome(curr.left, count);
+            pseudoPalindrome(curr.right, count);
+        }
+        count[curr.val]--;
+    }
+    public int pseudoPalindromicPaths (TreeNode root) {
+        pseudoPalindrome(root, new int[10]);
+        return ans;
+    }
+}
