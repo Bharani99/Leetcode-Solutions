87375742a9ddd29752f1f23af828c037eaf9120a
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-09-14 17:40:35 -0700

    Time: 630 ms (6.44%), Space: 135.5 MB (49.40%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,30 +1,19 @@
 class Solution {
-    
-    public void updateMap(HashMap<Integer, Integer> map, int num){
-        int temp;
-        temp = map.get(num) - 1;
-        if(temp == 0) map.remove(num);
-        else map.put(num, temp);
-    }
-    
-    public int[] findOriginalArray(int[] changed) {
-        if(changed.length % 2 == 1) return new int[0];
-        Arrays.sort(changed);
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] ans = new int[changed.length / 2];
-        int index = 0;
-        for(int num : changed) map.put(num, map.getOrDefault(num, 0) + 1);
-        for(int i = changed.length - 1; i >= 0; i--){
-            if(map.containsKey(changed[i])){
-                if(changed[i] % 2 == 1 || !map.containsKey(changed[i] / 2)) return new int[0];
-                if(map.containsKey(changed[i] / 2)){
-                    updateMap(map, changed[i]);
-                    updateMap(map, changed[i] / 2);
-                    ans[index++] = changed[i] / 2;
-                }
-                
+    public int[] findOriginalArray(int[] A) {
+        int n = A.length, i = 0;
+        if (n % 2 == 1) return new int[0];
+        int[] res = new int[n / 2];
+        Map<Integer, Integer> count = new TreeMap<>();
+        for (int a : A)
+            count.put(a, count.getOrDefault(a, 0) + 1);
+        for (int x : count.keySet()) {
+            if (count.get(x) > count.getOrDefault(x + x, 0))
+                return new int[0];
+            for (int j = 0; j < count.get(x); ++j) {
+                res[i++] = x;
+                count.put(x + x, count.get(x + x) - 1);
             }
         }
-        return map.isEmpty() ? ans : new int[0];
+        return res;
     }
 }
