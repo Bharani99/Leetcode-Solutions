c17978e5717ed90aafd036ced359f810c263fa26
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-09-15 23:56:50 -0700

    Time: 1051 ms (36.53%), Space: 691.1 MB (5.06%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,21 +1,16 @@
 class Solution {
-    
     public int maximise(int[] nums, int[] m, int left, int right, int index, int[][] dp){
         if(index == m.length) return 0;
-        
-        if(dp[left][index] != Integer.MIN_VALUE) return dp[left][index];
-        
+        if(dp[index][right] != Integer.MIN_VALUE) return dp[index][right];
         int ans = (nums[left] * m[index]) + maximise(nums, m, left + 1, right, index + 1, dp);
         ans = Math.max(ans, (nums[right] * m[index]) + maximise(nums, m, left, right - 1, index + 1, dp));
         
-        dp[left][index] = ans;
+        dp[index][right] = ans;
         return ans;
     }
-    
     public int maximumScore(int[] nums, int[] multipliers) {
-        int[][] dp = new int[nums.length][multipliers.length];
+        int[][] dp = new int[multipliers.length][nums.length];
         for(int[] d : dp) Arrays.fill(d, Integer.MIN_VALUE);
         return maximise(nums, multipliers, 0, nums.length - 1, 0, dp);
     }
-    
 }
