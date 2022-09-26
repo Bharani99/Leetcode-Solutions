14cffb31a422eb758ee810ed6b83bfbb93387c6b
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-09-25 18:16:06 -0700

    Time: 1 ms (100.00%), Space: 42.6 MB (49.65%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -1,58 +1,17 @@
-class Solution {
-    class Node{
-        char name;
-        Set<Character> equal, notEqual;
-        Node(char n){
-            name = n;
-            equal = new HashSet<>();
-            notEqual = new HashSet<>();
-        }
-    }
-    
-    public boolean traverse(Node curr, HashSet<Character> seen, HashSet<Character> notEqual, HashMap<Character, Node> map){
-        if( seen.contains(curr.name) )return true;
-        if( notEqual.contains(curr.name)) return false;
-        notEqual.addAll(curr.notEqual);
-        seen.add(curr.name);
-        for(char next : curr.equal){
-            if(!traverse(map.get(next), seen, notEqual, map)) return false;
-        }
-        return true;
-    }
-    
+class Solution {    int[] uf = new int[26];
     public boolean equationsPossible(String[] equations) {
-        HashMap<Character, Node> map = new HashMap<>();
-        char a, b;
-        HashSet<Character> set;
-        Node x, y;
-        for(String eq : equations){
-            a = eq.charAt(0);
-            b = eq.charAt(3);
-            x = map.getOrDefault(a, new Node(a));
-            y = map.getOrDefault(b, new Node(b));
-            if(eq.charAt(1) == '='){
-                x.equal.add(b);
-                y.equal.add(a);
-                if(x.notEqual.contains(b)) return false;
-            }
-            else {
-                if(a == b) return false;
-                x.notEqual.add(b);
-                y.notEqual.add(a);
-                if(x.equal.contains(b)) return false;
-            }
-            map.put(a, x);
-            map.put(b, y);
-        }
-        
-        HashSet<Character> seen = new HashSet<>();
-        
-        for(char eq : map.keySet()){
-            if(seen.size() == map.size()) break;
-            if(seen.contains(eq)) continue;
-            if(!traverse(map.get(eq), seen, new HashSet<>(), map)) return false;
-        }
-        
+        for (int i = 0; i < 26; ++i) uf[i] = i;
+        for (String e : equations)
+            if (e.charAt(1) == '=')
+                uf[find(e.charAt(0) - 'a')] = find(e.charAt(3) - 'a');
+        for (String e : equations)
+            if (e.charAt(1) == '!' && find(e.charAt(0) - 'a') == find(e.charAt(3) - 'a'))
+                return false;
         return true;
+    }
+
+    public int find(int x) {
+        if (x != uf[x]) uf[x] = find(uf[x]);
+        return uf[x];
     }
 }
