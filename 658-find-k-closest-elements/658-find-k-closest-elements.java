9ea2cabcdbf5830f955e6fb368621d769e4f0491
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-09-28 19:36:26 -0700

    Time: 27 ms (30.93%), Space: 63.9 MB (24.28%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -10,19 +10,18 @@ class Solution {
     }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int pos = binarySearch(arr, x);
-        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> dq = new LinkedList<>();
         int left = pos - 1, right = pos;
         while(k > 0){
             if(right >= arr.length)
-                dq.addFirst(arr[left--]);
-            else if(left < 0) dq.addLast(arr[right++]);
+                dq.add(0, arr[left--]);
+            else if(left < 0) dq.add(arr[right++]);
             else if(Math.abs(arr[left] - x) <= Math.abs(arr[right] - x))
-                dq.addFirst(arr[left--]);
-            else dq.addLast(arr[right++]);
+                dq.add(0, arr[left--]);
+            else dq.add(arr[right++]);
             k--;
         }
-        List<Integer> ans = new ArrayList<>();
-        while(!dq.isEmpty())ans.add(dq.poll());
-        return ans;
+        
+        return dq;
     }
 }
