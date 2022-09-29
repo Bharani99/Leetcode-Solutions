65b16cda4ef0804f3812d1f034efc732eba1a7c1
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-09-28 23:34:30 -0700

    Time: 23 ms (20.96%), Space: 51.6 MB (11.80%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,13 +1,11 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
+        int[] in = new int[numCourses];
         HashMap<Integer, HashSet<Integer>> dependant = new HashMap<>();
         
         HashSet<Integer> set;
         for(int[] prereq : prerequisites){
-            set = map.getOrDefault(prereq[0], new HashSet<>());
-            set.add(prereq[1]);
-            map.put(prereq[0], set);
+            in[prereq[0]]++;
             
             set = dependant.getOrDefault(prereq[1], new HashSet<>());
             set.add(prereq[0]);
@@ -16,7 +14,7 @@ class Solution {
         
         Queue<Integer> q = new LinkedList<>();
         for(int i = 0; i < numCourses; i++){
-            if(!map.containsKey(i)) q.add(i);
+            if(in[i] == 0) q.add(i);
         }
 
         int curr;
@@ -25,9 +23,8 @@ class Solution {
             curr = q.poll();
             ans.add(curr);
             for(int dep : dependant.getOrDefault(curr, new HashSet<>())){
-                set = map.get(dep);
-                set.remove(curr);
-                if(set.isEmpty()) q.add(dep);
+                in[dep]--;
+                if(in[dep] == 0) q.add(dep);
             }
         }
         return (ans.size() != numCourses) ? new int[0] : ans.stream().mapToInt(i -> i).toArray();
