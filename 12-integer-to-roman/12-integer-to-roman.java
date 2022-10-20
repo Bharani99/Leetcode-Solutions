3dba72b2c4a5bb690728ee8999b853ce3e31cddb
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-10-19 17:35:01 -0700

    Time: 9 ms (71.52%), Space: 45.4 MB (65.64%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -1,33 +1,21 @@
 class Solution {
-    public String intToRoman(int num) {
-        TreeMap<Integer, Character> map = new TreeMap<>();
-        map.put(1, 'I');
-        map.put(5, 'V');
-        map.put(10, 'X');
-        map.put(50, 'L');
-        map.put(100, 'C');
-        map.put(500, 'D');
-        map.put(1000, 'M');
+    public static String intToRoman(int num){
+        if (num < 1 || num > 3999) return "";
+
+        StringBuilder result = new StringBuilder();
+
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+        int i = 0;
         
-        String s = num + "";
-        int n = s.length() - 1, curr;
-        StringBuilder sb = new StringBuilder();
-        
-        for(char c : s.toCharArray()){
-            curr = Character.getNumericValue(c) * (int)(Math.pow(10, n --));
-            if(curr == 0) continue;
-            Integer higherKey = map.higherKey(curr);
-            
-            if(higherKey != null && 
-               (higherKey - curr == 1 || higherKey - curr == 10 || higherKey - curr == 100))
-                    sb.append(map.get(higherKey - curr) + "" + map.get(higherKey));
-            
-            else while(curr > 0){
-                Integer lowerKey = map.floorKey(curr);
-                sb.append(map.get(lowerKey));
-                curr -= lowerKey;
+        while (num >0) {
+            while ( num >= values[i]) {
+                num -= values[i];
+                result.append(roman[i]);
             }
+            i++;
         }
-        return sb.toString();
+        return result.toString();
     }
 }
