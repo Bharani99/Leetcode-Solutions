0ee6947fc869a9f96f1324600e97cd0d97e4da65
Bharani99 <31622653+Bharani99@users.noreply.github.com>
2022-10-22 14:34:35 -0700

    Time: 45 ms (20.14%), Space: 54 MB (16.83%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,55 +1,66 @@
 class Solution {
-    public int[] getHash(String t){
-        int[] hash = new int[52];
-        for(char c : t.toCharArray()){
-            if(c < 'a')hash[c - 'A']++;
-            else hash[c - 'a' + 26]++;
+    public String minWindow(String s, String t) {
+
+        if (s.length() == 0 || t.length() == 0) {
+            return "";
         }
-        return hash;
-    }
-    
-    public boolean validHash(int[] s, int[] t){
-        for(int i = 0; i < 52; i++){
-            if(s[i] < t[i]) return false;
+
+        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < t.length(); i++) {
+            int count = dictT.getOrDefault(t.charAt(i), 0);
+            dictT.put(t.charAt(i), count + 1);
         }
-        return true;
-    }
-    
-    public String minWindow(String S, String t) {
-        int m = t.length();
-        if(m > S.length()) return "";
-        int[] tHash = getHash(t);
-        int[] sHash = new int[52];
-        
-        int left = 0, right = 0;
-        char[] s = S.toCharArray();
-        char c;
-        int min = Integer.MAX_VALUE;
-        int minLeft = 0, minRight = 0;
-        
-        while(right < s.length){
-            c = s[right];
-            if(c < 'a') sHash[c - 'A']++;
-            else sHash[c - 'a' + 26]++;
-            
-            right++;
-            
-            while(right - left >= m){
-                if(validHash(sHash, tHash)){
-                    if(min > (right - left)){
-                        min = right - left;
-                        minRight = right;
-                        minLeft = left;
-                    }
-                    c = s[left];
-                    if(c < 'a') sHash[c - 'A']--;
-                    else sHash[c - 'a' + 26]--;
-                    left++;
-                }
-                else break;
+
+        int required = dictT.size();
+
+        // Filter all the characters from s into a new list along with their index.
+        // The filtering criteria is that the character should be present in t.
+        List<Pair<Integer, Character>> filteredS = new ArrayList<Pair<Integer, Character>>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (dictT.containsKey(c)) {
+                filteredS.add(new Pair<Integer, Character>(i, c));
             }
         }
-        
-        return S.substring(minLeft, minRight);
+
+        int l = 0, r = 0, formed = 0;
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+        int[] ans = { -1, 0, 0 };
+
+        // Look for the characters only in the filtered list instead of entire s.
+        // This helps to reduce our search.
+        // Hence, we follow the sliding window approach on as small list.
+        while (r < filteredS.size()) {
+            char c = filteredS.get(r).getValue();
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+
+            // Try and contract the window till the point where it ceases to be 'desirable'.
+            while (l <= r && formed == required) {
+                c = filteredS.get(l).getValue();
+
+                // Save the smallest window until now.
+                int end = filteredS.get(r).getKey();
+                int start = filteredS.get(l).getKey();
+                if (ans[0] == -1 || end - start + 1 < ans[0]) {
+                    ans[0] = end - start + 1;
+                    ans[1] = start;
+                    ans[2] = end;
+                }
+
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
     }
 }
